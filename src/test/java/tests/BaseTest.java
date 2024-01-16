0e597ef69f4b8c61028d625c34407ab731ec0a06
Dmitrii Elizarov <airplay1x6@gmail.com>
2024-01-16 19:52:46 +0300

    get autotest browser cloud

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -10,5 +10,6 @@ public class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 10000;
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
