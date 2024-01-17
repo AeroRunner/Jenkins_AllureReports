3ffd81ab7e394896b3c6e5b37d0f135fd88f48fd
Dmitrii Elizarov <airplay1x6@gmail.com>
2024-01-17 03:17:59 +0300

    add logs

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,7 +2,9 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +16,7 @@ public class BaseTest {
     @BeforeAll
     @DisplayName("Set settings before start tests ")
     public static void setUP() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         step(" Set remote browser", () -> {
             Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
                 });
@@ -33,6 +36,8 @@ public class BaseTest {
     @DisplayName("After every test we need reboot browser")
     void afterEveryTest(){
         Attach.screenshotAs("Last Screen");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
         step("Close Browser", ()->{
             Selenide.closeWebDriver();
         });
