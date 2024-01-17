7c532ab870405cc3156dcf2854d469ab97d9f760
Dmitrii Elizarov <airplay1x6@gmail.com>
2024-01-17 03:28:17 +0300

    add video

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -8,6 +8,10 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+import java.util.Objects;
 
 import static io.qameta.allure.Allure.step;
 
@@ -17,28 +21,38 @@ public class BaseTest {
     @DisplayName("Set settings before start tests ")
     public static void setUP() {
         SelenideLogger.addListener("allure", new AllureSelenide());
+
         step(" Set remote browser", () -> {
             Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-                });
+        });
         step("Set timeout(10000)", () -> {
 
-                    Configuration.timeout = 10000;
-                });
+            Configuration.timeout = 10000;
+        });
         step("Set baseUrl - https://demoqa.com", () -> {
-                    Configuration.baseUrl = "https://demoqa.com";
-                });
+            Configuration.baseUrl = "https://demoqa.com";
+        });
         step("Set browsersize(1920x1080)", () -> {
-                    Configuration.browserSize = "1920x1080";
-                });
+            Configuration.browserSize = "1920x1080";
+        });
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
 
     }
+
     @AfterEach
     @DisplayName("After every test we need reboot browser")
-    void afterEveryTest(){
+    void afterEveryTest() {
         Attach.screenshotAs("Last Screen");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        step("Close Browser", ()->{
+        Attach.addVideo();
+        step("Close Browser", () -> {
             Selenide.closeWebDriver();
         });
     }
